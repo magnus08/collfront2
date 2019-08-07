47b4936ca1cf3946413d66c9776a8c37e544603b
Magnus Andersson <magnus08@gmail.com>
2019-08-07 16:39:47 +0200

    Changed ip.

--- fetch/urls.js
+++ fetch/urls.js
@@ -1,5 +1,6 @@
 
-const baseUrl = "http://127.0.0.1:5000";
+// const baseUrl = "http://127.0.0.1:5000";
+const baseUrl = "http://10.200.200.2:5000";
 
 export const sensorOne = () => `${baseUrl}/sensor`;
 export const lastImage = () => `${baseUrl}/image`;
