687d752e0d15dc9732347c36d195393359e320f4
Magnus Andersson <magnus08@gmail.com>
2019-08-08 12:17:53 +0200

    Added express rest api layer.

---
+++ ssr-server.js
@@ -0,0 +1,39 @@
+
+const next = require('next');
+const express = require('express');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+.then(() => {
+  const server = express();
+
+  const baseUrl = "http://10.200.200.2:5000";
+  server.get('/api/sensor', (req, res) => {
+    const url = `${baseUrl}/sensor`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log("server fetch", data)
+    })
+    .catch(err => console.log("server fetch failed", err));
+    res.send({a: 1});
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  });
+
+  server.listen(3000, (err) => {
+    if(err)  {
+      throw err;
+    }
+    console.log('> Ready on http://localhost:3000')
+  })
+})
+.catch((ex) => {
+  console.error(ex.stack);
+  process.exit(1);
+});
