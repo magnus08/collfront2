72ce043ce2dafa1d5fabc4c9d0a27027a8921f06
Magnus Andersson <magnus08@gmail.com>
2019-08-08 12:20:45 +0200

    Ports.

--- fetch/urls.js
+++ fetch/urls.js
@@ -5,8 +5,8 @@ const baseUrl = "http://10.200.200.2:5000";
 export const sensorOne = () => `${baseUrl}/sensor`;
 export const lastImage = () => `${baseUrl}/image`;
 
-// const baseUrlLocal = "http://noll.no-ip.info:32147";
-const baseUrlLocal = "http://localhost:3000";
+const baseUrlLocal = "http://noll.no-ip.info:32147";
+// const baseUrlLocal = "http://localhost:3000";
 
 export const sensorOneLocal = () => `${baseUrlLocal}/api/sensor`;
 export const lastImageLocal = () => `${baseUrlLocal}/api/image`;
--- ssr-server.js
+++ ssr-server.js
@@ -26,11 +26,11 @@ app.prepare()
     return handle(req, res)
   });
 
-  server.listen(3000, (err) => {
+  server.listen(32147, (err) => {
     if(err)  {
       throw err;
     }
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:32147')
   })
 })
 .catch((ex) => {
